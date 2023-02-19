d831b24559b4c66197e1ba5c81cec11c47bfcd89
Latira <l@g.com>
2023-02-19 09:35:45 +0300

    init: final version

--- src/index.js
+++ src/index.js
@@ -37,9 +37,30 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const bin_table = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+  };
+
 function decode(expr) {
-    // write your solution here
+    let result = '', bin_code ='';
+    for (let i=0; i<expr.length; i=i+10) {
+      morse_code = '';
+      bin_code = expr.substring(i,i+10);
+      if (bin_code != '**********') {
+        for (let j=0; j<10; j=j+2) {
+          bin_element = bin_code.substring(j,j+2);
+          morse_code = morse_code + bin_table[bin_element];
+        }
+        result = result + MORSE_TABLE[morse_code];
+      } else {
+        result = result + ' ';
+      }   
+    }
+    return result;    
 }
+
 
 module.exports = {
     decode
